910aede199063d37920eb77436c2a716f3a4cc78
douglasswm <douglas.cst@rub.edu.bt>
2025-04-22 10:44:53 +0600

    wip: setUser state into store from service layer

--- shared/services/userService.tsx
+++ shared/services/userService.tsx
@@ -7,19 +7,32 @@
 // https://firebase.google.com/docs/firestore/query-data/get-data#web
 import app from "@/lib/firebaseConfig";
 import { getFirestore } from "@react-native-firebase/firestore";
+import { useStore } from "../store";
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 // Build a reference to the collection and document
 const userCollection = db.collection("users");
 
 async function getUser() {
-  // const users = await db().collection("users").get();
+  // 1. PRESENTATION LAYER -> CALL SERVICE LAYER GETUSER()
+  // 2. SERVICE LAYER -> READ USER DATA FROM DATABASE LAYER
+  // 3. PERSISTENCE LAYER -> RETURN USER DATA FROM DATABSE LAYER
+  // AND SAVE TO PERSISTENCE LAYER (STORE:ZUSTAND LIBRARY)
+  // 4. PRESENTATION LAYER -> USE PERSISTENCE LAYER CALLING USE STATE TO DISPLAY USER DATA
+
+  // REQUESTING USER DATA BY ID FROM DATABASE FIRESTORE
   const response = await userCollection
     .doc("HJzX8Mb5mvO5mviSdSZ2")
     .get()
     .then((doc) => {
+      // VERIFYING DATA EXIST
       if (doc.exists) {
         console.log("Document data:", doc.data());
+        // UPDATE DATABASE RESPONSE TO STORE
+        const setUser = useStore((state) => state.updateUser(doc.data()));
+        console.log("setUser", setUser);
+
+        // RETURN USER DATA
         return doc.data();
       }
     });
